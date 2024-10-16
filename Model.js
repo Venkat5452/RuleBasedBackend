dbeb9a26981f063431557cb25ac3b7f4cd0a5617
Venkat5452 <venkatsai.bandi2019@gmail.com>
2024-10-16 19:24:17 +0530

    Delete Model.js

--- Model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const RuleSchema = new mongoose.Schema({
-    ruleString: { type: String, required: true },
-    ast: { type: Object, required: true },
-    createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Rule', RuleSchema);
